3762b187b0c87247aa39bffb63749b297e415c86
Rafael Xavier de Souza <rxaviers@gmail.com>
2012-05-04 18:06:43 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/* ==========================================================
+ * Spa nefertiti page.
+ *
+ * Dual licensed under the MIT and GPL Version 2 licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * Copyright (c) 2012 Rafael Xavier de Souza <rxaviers@gmail.com>.
+ * ========================================================== 
+ */
+;
+(function( $, undefined ) {
+
+})(jQuery);
+
